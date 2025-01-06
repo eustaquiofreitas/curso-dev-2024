9fe10b9f3058aaac13b3ba6cbe57092e64496f25
eustaquiofreitas <eustaquio.efb@gmail.com>
2025-01-05 22:22:18 -0300

    Criação do Projeto

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home(){
+    return <div>
+        <h1>Curso de Programação Curso Dev 2024.</h1>
+        <h2>Treinamento em programação.</h2>
+        <br />
+        <button>Acessar aqui...</button>
+    </div>
+}
+
+export default Home
