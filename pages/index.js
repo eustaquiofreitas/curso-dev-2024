4b5e2cc8d47a365faa33a6def26512e5d3155b28
eustaquiofreitas <eustaquio.efb@gmail.com>
2025-01-30 16:53:09 -0300

    Alterando o Projeto

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,9 @@ function Home(){
     return <div>
         <h1>Curso de Programação Curso Dev 2024.</h1>
         <h2>Treinamento em programação.</h2>
+        <h3>Linha Acrescentada.</h3>
         <br />
         <button>Acessar aqui...</button>
     </div>
 }
-
-function onClick() {
-    let dado = prompt("Digite O valor")
-    return dado
-}
-
-onclick()
 export default Home
